65c495663db493fab53b4fa03219dba30244d57d
wcs <1103409364@qq.com>
2022-05-18 15:19:50 +0800

    fix: 入口 main
    Signed-off-by: wcs <1103409364@qq.com>

---
+++ src/pages/index/main.ts
@@ -0,0 +1,4 @@
+const app = document.querySelector<HTMLDivElement>("#app");
+app && (app.innerHTML = "首页");
+// 无法在 "--isolatedModules" 下编译“main.ts”，因为它被视为全局脚本文件。请添加导入、导出或空的 "export {}" 语句来使它成为模块。ts(1208)
+export {};
---
+++ src/pages/page-1/main.ts
@@ -0,0 +1,4 @@
+const app = document.querySelector<HTMLDivElement>("#app");
+app && (app.innerHTML = "page-1 111");
+// 无法在 "--isolatedModules" 下编译“main.ts”，因为它被视为全局脚本文件。请添加导入、导出或空的 "export {}" 语句来使它成为模块。ts(1208)
+export {};
