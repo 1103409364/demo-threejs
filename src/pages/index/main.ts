0ff3332b943cb65b453454513f89ed7a1a2c86c7
wcs <1103409364@qq.com>
2022-05-18 19:21:43 +0800

    fix: 首页导航自动生成
    Signed-off-by: wcs <1103409364@qq.com>

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,4 +1,23 @@
 const app = document.querySelector<HTMLDivElement>("#app");
-app && (app.innerHTML = "首页");
+
+app && (app.innerHTML = getPages());
+
+// 自动导入页面，生成导航列表
+function getPages() {
+  const pages = import.meta.globEager("/src/pages/**/*.ts"); // 只能导入 ts 文件？
+  let temp = "<ul>";
+  for (const key in pages) {
+    const dirName = key.match(/\/src\/pages\/([\w-]+)\/\w+\.ts/)?.[1]; // 匹配出页面名称
+    let pageName = "index";
+    // index 此时还未导出
+    if (dirName !== "index") {
+      pageName = pages[key].pageInfo.title;
+      temp += `<li><a href="/${dirName}">${pageName}</a></li>`;
+    }
+  }
+  temp += "</ul>";
+  return temp;
+}
+
 // 无法在 "--isolatedModules" 下编译“main.ts”，因为它被视为全局脚本文件。请添加导入、导出或空的 "export {}" 语句来使它成为模块。ts(1208)
-export {};
+export const pageInfo = { title: "首页" };
--- src/pages/page-1/main.ts
+++ src/pages/page-1/main.ts
@@ -1,4 +1,59 @@
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  LineBasicMaterial,
+  Vector3,
+  BufferGeometry,
+  Line,
+} from "three";
+
 const app = document.querySelector<HTMLDivElement>("#app");
-app && (app.innerHTML = "page-1 111");
-// 无法在 "--isolatedModules" 下编译“main.ts”，因为它被视为全局脚本文件。请添加导入、导出或空的 "export {}" 语句来使它成为模块。ts(1208)
-export {};
+// 场景
+const scene = new Scene();
+// 相机 透视相机 近大远小
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// 创建渲染器
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+app && app.appendChild(renderer.domElement);
+
+const cube = createCube();
+const line = createLine();
+scene.add(cube);
+scene.add(line);
+
+camera.position.z = 5;
+
+// 创建一个立方体
+function createCube() {
+  const geometry = new BoxGeometry();
+  const material = new MeshBasicMaterial({ color: 0x00ff00 }); // 定义纹理材质
+  const cube = new Mesh(geometry, material);
+  return cube;
+}
+// 创建一条线
+function createLine() {
+  const material = new LineBasicMaterial({ color: 0x00ff00 }); // 定义纹理材质
+  const points = [];
+  points.push(new Vector3(-10, 0, 0));
+  points.push(new Vector3(0, 10, 0));
+  points.push(new Vector3(10, 0, 0));
+
+  const geometry = new BufferGeometry().setFromPoints(points); // 定义几何体
+  const line = new Line(geometry, material);
+  return line;
+}
+// 渲染
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
+
+export const pageInfo = { title: "创建一个场景（Creating a scene）" };
