3b71d95510c304ea6f53e986dba9c1466e9cccb7
wcs <1103409364@qq.com>
2022-05-19 09:43:46 +0800

    chore: legacy config
    Signed-off-by: wcs <1103409364@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import { defineConfig } from "vite";
+// import legacy from "@vitejs/plugin-legacy";
 import mpa from "vite-plugin-mpa";
 
 // * @see https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mpa({ open: "/" })],
+  plugins: [
+    mpa({ open: "/" }),
+    // legacy({
+    //   targets: ["ie >= 11"],
+    //   additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    // }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
