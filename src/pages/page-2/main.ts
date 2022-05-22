8a86b4b2a163cad5b420a5362337047953b21482
1103409364 <1103409364@qq.com>
2022-05-22 13:05:00 +0800

    demo2 scene camara
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/pages/page-2/main.ts
+++ src/pages/page-2/main.ts
@@ -1,17 +1,73 @@
-import { WordCount } from "../../components/WordCount";
+import "@/style/index.scss";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// Define the new element
-customElements.define("word-count", WordCount, { extends: "p" });
+const c1 = document.querySelector<HTMLCanvasElement>("#c1");
+const c2 = document.querySelector<HTMLCanvasElement>("#c2");
+const c3 = document.querySelector<HTMLCanvasElement>("#c3");
+const c4 = document.querySelector<HTMLCanvasElement>("#c4");
+// 场景
+const scene = new THREE.Scene();
+// 相机 透视相机 近大远小
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+// 相机2 正交相机
+const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 5);
+const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 5);
+const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 5);
+camera1.position.z = 2;
+camera2.position.y = 2;
+camera2.lookAt(new THREE.Vector3(0, 0, 0));
+camera3.position.z = 2;
+camera4.position.x = 2;
+camera4.lookAt(new THREE.Vector3(0, 0, 0));
 
-const template = `
-<h1>Word count rating widget</h1>
-<article contenteditable="">
-  <h2>Sample heading</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar sed justo sed viverra. Aliquam ac scelerisque tellus. Vivamus porttitor nunc vel nibh rutrum hendrerit. Donec viverra vestibulum pretium. Mauris at eros vitae ante pellentesque bibendum. Etiam et blandit purus, nec aliquam libero. Etiam leo felis, pulvinar et diam id, sagittis pulvinar diam. Nunc pellentesque rutrum sapien, sed faucibus urna sodales in. Sed tortor nisl, egestas nec egestas luctus, faucibus vitae purus. Ut elit nunc, pretium eget fermentum id, accumsan et velit. Sed mattis velit diam, a elementum nunc facilisis sit amet.</p>
-  <p>Pellentesque ornare tellus sit amet massa tincidunt congue. Morbi cursus, tellus vitae pulvinar dictum, dui turpis faucibus ipsum, nec hendrerit augue nisi et enim. Curabitur felis metus, euismod et augue et, luctus dignissim metus. Mauris placerat tellus id efficitur ornare. Cras enim urna, vestibulum vel molestie vitae, mollis vitae eros. Sed lacinia scelerisque diam, a varius urna iaculis ut. Nam lacinia, velit consequat venenatis pellentesque, leo tortor porttitor est, sit amet accumsan ex lectus eget ipsum. Quisque luctus, ex ac fringilla tincidunt, risus mauris sagittis mauris, at iaculis mauris purus eget neque. Donec viverra in ex sed ullamcorper. In ac nisi vel enim accumsan feugiat et sed augue. Donec nisl metus, sollicitudin eu tempus a, scelerisque sed diam.</p>
-  <p is="word-count"></p>
-</article>`;
-const app = document.querySelector<HTMLDivElement>("#app");
-app && (app.innerHTML = template);
-// 无法在 "--isolatedModules" 下编译“main.ts”，因为它被视为全局脚本文件。请添加导入、导出或空的 "export {}" 语句来使它成为模块。ts(1208)
-export const pageInfo = { title: "web-component" };
+// 创建渲染器
+const renderer1 = c1 && new THREE.WebGLRenderer({ canvas: c1 });
+const renderer2 = c2 && new THREE.WebGLRenderer({ canvas: c2 });
+const renderer3 = c3 && new THREE.WebGLRenderer({ canvas: c3 });
+const renderer4 = c4 && new THREE.WebGLRenderer({ canvas: c4 });
+renderer1?.setSize(200, 200);
+renderer2?.setSize(200, 200);
+renderer3?.setSize(200, 200);
+renderer4?.setSize(200, 200);
+// app?.appendChild(renderer1.domElement);
+
+const controls1 = c1 && new OrbitControls(camera1, c1);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// window.addEventListener("resize", onWindowResize, false);
+// function onWindowResize() {
+//   camera1.aspect = window.innerWidth / window.innerHeight;
+//   camera1.updateProjectionMatrix();
+//   renderer1.setSize(window.innerWidth, window.innerHeight);
+//   render();
+// }
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+
+  controls1?.update();
+
+  render();
+}
+
+function render() {
+  renderer1?.render(scene, camera1);
+  renderer2?.render(scene, camera2);
+  renderer3?.render(scene, camera3);
+  renderer4?.render(scene, camera4);
+}
+animate();
+
+export const pageInfo = { title: "TypeScript-Boilerplate" };
