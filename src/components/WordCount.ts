438ab1361e669a4b5e443c898731f9b34d773ed1
1103409364 <1103409364@qq.com>
2022-05-22 11:06:20 +0800

    refactor: countWords
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/components/WordCount.ts
+++ src/components/WordCount.ts
@@ -1,39 +1,47 @@
 // Create a class for the element
 export class WordCount extends HTMLParagraphElement {
+  // count words in element's parent element
+  private wcParent = this.parentNode;
+  private textElement;
+
   constructor() {
     // Always call super first in constructor
     super();
-
-    // count words in element's parent element
-    const wcParent = this.parentNode;
-    // count words in element's text
-    function countWords(node: ParentNode | null) {
-      const text = node?.textContent || "";
-      return text
-        .trim()
-        .split(/\s+/g)
-        .filter((a) => a.trim().length > 0).length;
-    }
-
-    const count = `Words: ${countWords(wcParent)}`;
-
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
-
     // Create text node and add word count to it
-    const text = document.createElement("span");
-    text.textContent = count;
-
+    this.textElement = document.createElement("span");
     // Append it to the shadow root
-    shadow.appendChild(text);
+    shadow.appendChild(this.textElement);
 
-    // Update count when element content changes
-    setInterval(function () {
-      const count = `Words: ${countWords(wcParent)}`;
-      text.textContent = count;
-    }, 500);
+    this.init();
+  }
+  init() {
+    this.addEvent();
+    this.addStyle();
+    this.updateCount();
+  }
+  addStyle() {
+    this.textElement.style.color = "red";
+  }
+  addEvent() {
+    this.wcParent?.addEventListener("input", this.updateCount.bind(this));
+  }
+  // count words in element's text
+  countWords() {
+    const text = this.wcParent?.textContent || "";
+    return text
+      .trim()
+      .split(/\s+/g)
+      .filter((a) => a.trim().length > 0).length;
+  }
+  updateCount() {
+    const count = `Words: ${this.countWords()}`;
+    this.textElement.textContent = count;
   }
 }
-
+// Define the new element
+customElements.define("word-count", WordCount, { extends: "p" });
 // https://developer.mozilla.org/zh-CN/docs/Web/Web_Components#%E4%BE%8B%E5%AD%90
 // https://github.com/mdn/web-components-examples
+// https://developer.mozilla.org/zh-CN/docs/Web/Web_Components/Using_shadow_DOM
