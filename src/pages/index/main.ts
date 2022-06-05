522d8d32a4bf2b85e1914eed7525f88c427cc81b
1103409364 <1103409364@qq.com>
2022-06-05 20:38:09 +0800

    fix: globEager 导入立即执行改用 glob
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -5,16 +5,11 @@ app && (app.innerHTML = getPages());
 
 // 自动导入页面，生成导航列表
 function getPages() {
-  const pages = import.meta.globEager("/src/pages/**/*.ts"); // 只能导入 ts 文件？
+  const pages = import.meta.glob("/src/pages/**/*.ts"); // 只能导入 ts 文件？
   let temp = "<ul>";
   for (const key in pages) {
     const dirName = key.match(/\/src\/pages\/(.+)\/\w+\.ts/)?.[1]; // 匹配出页面名称
-    let pageName = "index";
-    // index 此时还未导出
-    if (dirName !== "index" && pages[key].pageInfo) {
-      pageName = pages[key].pageInfo.title;
-      temp += `<li><a href="/${dirName}">${pageName}</a></li>`;
-    }
+    !dirName?.includes("index") && (temp += `<li><a href="/${dirName}">${dirName}</a></li>`);
   }
   temp += "</ul>";
   return temp;
