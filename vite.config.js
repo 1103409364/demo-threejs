bf4e4d44fc2bc85a5dfde673135a051e4f4716b8
wcs <1103409364@qq.com>
2023-04-08 10:17:38 +0800

    pnpm up --latest
    Signed-off-by: wcs <1103409364@qq.com>

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import glsl from "vite-plugin-glsl";
 // * @see https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    mpa({ open: "/index.html" }),
+    mpa({ open: "/src/pages/index.html" }),
     glsl(),
     // legacy({
     //   targets: ["ie >= 11"],
